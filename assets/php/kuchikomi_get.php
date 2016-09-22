7f291511998f733de1ab7cdd17ea54b5d66ead62
yuskm <380pinkmoon053@gmail.com>
2016-09-22 13:05:04 +0900

    no message

--- assets/php/kuchikomi_get.php
+++ assets/php/kuchikomi_get.php
@@ -3,10 +3,8 @@
 
     // PostgreSQL Connect
     $url = parse_url(getenv('DATABASE_URL'));
-    $str = "host=" . $url['host'] . "user=" . $url['user'] . "password=" . $url['pass'] . "dbname=" . substr($url['path'], 1);
-
     $dbconn = pg_connect("host=" . $url['host'] . " user=" . $url['user'] . " password=" . $url['pass'] . " dbname=" . substr($url['path'], 1));
-/*
+
     // SQL
     $sql = "SELECT * FROM bihakunavi;";
     $result = pg_query($dbconn, $sql);
@@ -19,14 +17,9 @@
     }
 
     // PostgreSQL Disconnect
-*/
     pg_close( $dbconn );
 
-//    header( 'Content-Type: application/json; charset=utf-8' );
-//    echo json_encode( $data );
+    header( 'Content-Type: application/json; charset=utf-8' );
+    echo json_encode( $data );
 
-    echo ($url);
-    var_dump($url);
-    var_dump($dbconn);
-    var_dump($str);
 ?>
